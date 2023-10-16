62edf573e2cbb1e2e6e95481617f5892b2508eb2
CETerry <randomcretin@hotmail.com>
2023-10-16 15:08:56 -0500

    Added the beginning of the functions

---
+++ functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void print_to_screen() {
+    std::cout << "Try typing this." << std::endl;
+}
+
+std::string take_input() {
+    std::string input;
+    std::cin >> input;
+    return input;
+}
+
+
+int main() {
+    print_to_screen();
+    std::cout << take_input() << std::endl;
+
+    return 0;
+}
